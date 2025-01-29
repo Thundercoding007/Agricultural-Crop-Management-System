5acf7ef7521ab91594973d99e37a0721f806aa2a
Thundercoding007 <lalitvpatil2005@gmail.com>
2025-01-29 19:26:15 +0530

    Delete Frontend directory

--- Frontend/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.scroll-link').forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevent the default link behavior
-            var targetId = this.getAttribute('data-target');
-            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
-        });
-    });
-});
